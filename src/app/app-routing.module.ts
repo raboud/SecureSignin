993d78a88cba9f254bf4cf96724f4f19a6e938de
Robert Raboud <robert.raboud@randreng.com>
2019-05-07 22:40:32 -0400

    Set to use Secure Common

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AuthGuard } from 'src/common/app/Services/auth.guard';
+import { LoginComponent } from 'src/common/app/Components/login/login.component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
+
+const routes: Routes = [
+  {
+    path: '', canActivate: [AuthGuard], children: [
+      { path: 'dashboard', component: DashboardComponent },
+    ]},
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: 'dashboard', pathMatch: 'full' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+
+import { SecureCommonModule } from 'src/common/app/secure-common.module';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule,
+    RouterModule,
+    SecureCommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
